4c560a7b38c307dbafd6d820d14de144d57bbe00
Meatssauce <70827089+Meatssauce@users.noreply.github.com>
2022-09-11 18:42:32 +1000

    initalised my brunch as added some empty functions

---
+++ utils.py
@@ -0,0 +1,43 @@
+# new/good opportunity that's been neglected for too long
+# alarm for having too many opportunities or core opportunities
+
+# total opportunity value formula:
+# f(how much I like them, whether they are a central node,
+# how much they may contribute to their specialty * how much weight I am putting on said specialty)
+
+# measure: rate of lead creation; distribution of lead, opportunity and closed; close rate; closed lost rate and closed won rate;
+# general efficiency
+
+# track current close friend separately
+# closed won != current close friend because a close friend may preexist or they may leave ones life because of unrelated reasons.
+
+# auto suggest cutting loses
+
+
+def neglected():
+    # opportunity is worth-while but has been neglected for too long
+    pass
+
+
+def overload():
+    # too many leads/opportunities/active close friends
+    pass
+
+
+def total_thread_value():
+    pass
+
+
+def monthly_thread_value():
+    # how happy I am when around them NOW,
+    # use data from n most recent + n extrapolated future
+    pass
+
+
+def popular_kid():
+    # return (has above avg connections)
+    pass
+
+
+def get_metrics():
+    pass
