e9f5363e871d5f5e3ef8bb6d3ad44a70892d9791
shaneSleeman <shanesleeman1@gmail.com>
2022-09-15 09:59:31 +1000

    Basic form layout

--- src/AddFriend.js
+++ src/AddFriend.js
@@ -1,31 +1,49 @@
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
 function AddFriend() {
   return (
     <>
-      <h1>NEW FRIEND</h1>
-      <Form>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Control type="email" placeholder="Name" />
-        </Form.Group>
+      <Container>
+        <Row>
+          <Col></Col>
+          <Col sm={12} md={8} lg={6}>
+            <h1>NEW FRIEND</h1>
+            <Form>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Control type="email" placeholder="Name" />
+              </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Control type="password" placeholder="First Met At" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Control type="password" placeholder="First Met On" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Control type="password" placeholder="Contact Info" />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Clear
-        </Button>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Control type="password" placeholder="First Met At" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Control type="password" placeholder="First Met On" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Control type="password" placeholder="Contact Info" />
+              </Form.Group>
+              <Row>
+                <Col>
+                  <Button variant="danger" type="submit">
+                    Clear
+                  </Button>
+                </Col>
+                <Col className="align-items-end">
+                  <Button variant="success" type="submit">
+                    Submit
+                  </Button>
+                </Col>
+              </Row>
+            </Form>
+          </Col>
+          <Col></Col>
+        </Row>
+      </Container>
     </>
   );
 }
