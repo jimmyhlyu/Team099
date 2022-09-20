dfe977b74102c522386c4bf5ba686e57eb2dde11
shaneSleeman <shanesleeman1@gmail.com>
2022-09-20 12:10:15 +1000

    Rename buttons

--- src/Login.js
+++ src/Login.js
@@ -7,10 +7,10 @@ function Login() {
       <img src={Logo} className="logo"></img>
       <div className="darkBox">
         <p className="message">Welcome to FSK!</p>
-        <input type="text" name="" placeholder="Username"></input>
-        <input type="password" name="" placeholder="Password"></input>
+        <input type="text" name="username" placeholder="Username"></input>
+        <input type="password" name="password" placeholder="Password"></input>
         <bottonc className="button">Login</bottonc>
-        <botton className="button">SignUp</botton>
+        <botton className="button">Signup</botton>
       </div>
     </>
   );
