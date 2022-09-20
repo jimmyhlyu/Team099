86402ab23aa0f50b39a07bf84a4fccb2f47a1033
Meatssauce <70827089+Meatssauce@users.noreply.github.com>
2022-09-20 15:34:07 +1000

    updated data structure and default values

--- database/data_classes.py
+++ database/data_classes.py
@@ -1,151 +1,104 @@
 from dataclasses import dataclass, field
 from datetime import datetime, timedelta
 from .account_status import AccountType
+from uuid import uuid4, UUID
 
 
 @dataclass
 class User:
     """Class for keeping track of users."""
-
-    id_: str
+    
     name: str
     account_status: AccountType
-    network_id: str
-    social_preference_id: str
+    network_id: UUID
+    social_preference_id: UUID
 
-    lead_conversion_rate: int
-    avg_lead_quality: float
-    opportunity_conversion_rate: float
-    avg_opportunity_quality: float
-    close_rate: float
-    win_rate: float
-    lose_rate: float
-    vetting_accuracy: float
+    id_: UUID = field(default_factory=lambda: uuid4())
+    stats: dict = field(default_factory=lambda: {
+        'lead_conversion_rate': 0,
+        'avg_lead_quality': 0,
+        'opportunity_conversion_rate': 0,
+        'avg_opportunity_quality': 0,
+        'close_rate': 0,
+        'win_rate': 0,
+        'lose_rate': 0,
+        'vetting_accuracy': 0,
+    })
 
     @staticmethod
     def from_dict(source):
-        return User(
-            source['id'],
-            source['name'],
-            source['account_status'],
-            source['network_id'],
-            source['social_preference_id'],
-            source['lead_conversion_rate'],
-            source['avg_lead_quality'],
-            source['opportunity_conversion_rate'],
-            source['avg_opportunity_quality'],
-            source['close_rate'],
-            source['win_rate'],
-            source['lose_rate'],
-            source['vetting_accuracy'],
-        )
+        return User(**source)
 
     def to_dict(self):
         return {
-            'id': self.id_,
             'name': self.name,
             'account_status': self.account_status,
             'network_id': self.network_id,
             'social_preference_id': self.social_preference_id,
-            'lead_conversion_rate': self.lead_conversion_rate,
-            'avg_lead_quality': self.avg_lead_quality,
-            'opportunity_conversion_rate': self.opportunity_conversion_rate,
-            'avg_opportunity_quality': self.avg_opportunity_quality,
-            'close_rate': self.close_rate,
-            'win_rate': self.win_rate,
-            'lose_rate': self.lose_rate,
-            'vetting_accuracy': self.vetting_accuracy,
+            'id': self.id_,
+            'stats': self.stats,
         }
 
 
 @dataclass
 class Connection:
     """Class for keeping track of connections."""
-
-    id_: str
+    
     name: str
-    network_id: str
+    network_id: UUID
     sex: str
     age: int
-    date_first_met: datetime
 
-    priority: int  # ranking of expected_value in the network
-    expected_value: float  # probability_of_success * value_to_user
+    id_: UUID = field(default_factory=lambda: uuid4())
+    date_first_met: datetime = datetime.today()
+    hours_spent_together: float = 0
 
-    probability_of_success: float  # statistical value (internal and external factors)
-    value: float  # user-centric value
+    priority: int = 0  # ranking of expected_value in the network
+    expected_value: float = 0  # probability_of_success * value_to_user
+
+    probability_of_success: float = 0.5  # statistical value (internal and external factors)
+    value: float = 0  # user-centric value
 
     # value = metric_weights * metric_scores
-
-    charm: float
-    closeness: float
-    companionship: float
-    usefulness: float
-    how_much_they_like_us: float
-    non_toxicity: float
-    responsiveness: float  # includes proactiveness
-    external_difficulty: float
-    nexus: bool
-    popularity: str
-    time_spent_together: timedelta
+    metrics: dict = field(default_factory=lambda: {
+        'charm': 0,
+        'closeness': 0,
+        'companionship': 0,
+        'usefulness': 0,
+        'how_much_they_like_us': 0,
+        'toxicity': 0,
+        'responsiveness': 0,  # includes proactiveness
+        'external_difficulty': 0,
+        'nexus': 0,
+        'popularity': 0,
+    })
 
     @staticmethod
     def from_dict(source):
-        return Connection(
-            source['id'],
-            source['name'],
-            source['network_id'],
-            source['sex'],
-            source['age'],
-            source['date_first_met'],
-            source['priority'],
-            source['expected_value'],
-            source['probability_of_success'],
-            source['value'],
-            source['charm'],
-            source['closeness'],
-            source['companionship'],
-            source['usefulness'],
-            source['how_much_they_like_us'],
-            source['non_toxicity'],
-            source['responsiveness'],
-            source['external_difficulty'],
-            source['nexus'],
-            source['popularity'],
-            source['time_spent_together']
-        )
+        return Connection(**source)
 
     def to_dict(self):
         return {
-            'id': self.id_,
             'name': self.name,
             'network_id': self.network_id,
             'sex': self.sex,
             'age': self.age,
+            'id': self.id_,
             'date_first_met': self.date_first_met,
+            'hours_spent_together': self.hours_spent_together,
             'priority': self.priority,
             'expected_value': self.expected_value,
             'probability_of_success': self.probability_of_success,
             'value': self.value,
-            'charm': self.charm,
-            'closeness': self.closeness,
-            'companionship': self.companionship,
-            'usefulness': self.usefulness,
-            'how_much_they_like_us': self.how_much_they_like_us,
-            'non_toxicity': self.non_toxicity,
-            'responsiveness': self.responsiveness,
-            'external_difficulty': self.external_difficulty,
-            'nexus': self.nexus,
-            'popularity': self.popularity,
-            'time_spent_together': self.time_spent_together,
+            'metrics': self.metrics,
         }
 
 
 @dataclass
 class SocialPreference:
-    id_: str
-    user_id: str
-    last_confirmed: timedelta
+    user_id: UUID
+    last_edited: datetime = datetime.today()
+    id_: UUID = field(default_factory=lambda: uuid4())
     sector_weights: dict = field(default_factory=lambda: {'friendship': 1.0, 'relationship': 1.0, 'career': 1.0})
     metric_weights: dict = field(default_factory=lambda: {
         'charm': 1.0,
@@ -153,7 +106,20 @@ class SocialPreference:
         'companionship': 1.0,
         'usefulness': 1.0,
         'how_much_they_like_us': 1.0,
-        'non_toxicity': 1.0
+        'toxicity': -1.0
     })
+
+    @staticmethod
+    def from_dict(source):
+        return SocialPreference(**source)
+
+    def to_dict(self):
+        return {
+            'id': self.id_,
+            'user_id': self.user_id,
+            'last_edited': self.last_edited,
+            'sector_weights': self.sector_weights,
+            'metric_weights': self.metric_weights,
+        }
 
 
