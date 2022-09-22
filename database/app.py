01401bff5abedf324f576556aea433d4abadb0d7
Meatssauce <70827089+Meatssauce@users.noreply.github.com>
2022-09-22 20:22:55 +1000

    implemented flask app; renamed compute.py to app.py

--- database/app.py
+++ database/app.py
@@ -2,19 +2,41 @@ from datetime import datetime, timedelta
 
 import pandas as pd
 import numpy as np
+from google.cloud.firestore_v1 import DocumentSnapshot
+
 from database.data_classes import User, Connection
 from database.helpful_classes import db
-
-import firebase_admin
-from firebase_admin import credentials
+from json import loads, dumps
 from firebase_admin import firestore
 
+from flask import Flask
+from flask import request
+from markupsafe import escape
 
-def add_user(user_dict: dict):
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello() -> str:
+    return "Hello, World!"
+
+
+@app.route('/user/get/<string:user_id>')
+def get_user(user_id: str) -> str:
+    return dumps(db.collection('users').document(user_id).get())
+
+
+@app.route('/user/add/<string:user_dict>')
+def add_user(user_dict: dict | str):
+    if isinstance(user_dict, str):
+        user_dict = loads(user_dict)
+
     user = User.from_dict(user_dict)
     db.collection('users').document(user.id).set(user.to_dict())
 
 
+@app.route('/user/delete/<string:user_id>')
 def delete_user(user_id: str):
     # delete user's connections
     docs = db.collection('connectionHistory').where('connection.user_id', '==', user_id).stream()
@@ -26,7 +48,11 @@ def delete_user(user_id: str):
     db.collection('users').document(user_id).delete()
 
 
-def update_user(user_id: str, params_dict: dict):
+@app.route('/user/update/<string:user_id>/<string:params_dict>')
+def update_user(user_id: str, params_dict: dict | str):
+    if isinstance(params_dict, str):
+        params_dict = loads(params_dict)
+
     db.collection('users').document(user_id).update({f'{k}': v for k, v in params_dict.items()})
 
 
@@ -37,7 +63,28 @@ def get_latest_weights(user_id: str):
     return sector_weights, metric_weights
 
 
-def add_connection(user_id: str, connection_dict: dict):
+@app.route('/connection/get/<string:connection_id>')
+def get_connection(connection_id: str) -> str:
+    return dumps(_get_connection(connection_id).to_dict())
+
+
+def _get_connection(connection_id: str) -> DocumentSnapshot:
+    docs = db.collection('connectionHistory').where('connection.id', '==', connection_id).stream()
+    return max(docs, key=lambda x: x.to_dict()['timestamp'])
+
+
+@app.route('/connection/get/from_user/<string:user_id>')
+def get_connections_from(user_id: str) -> str:
+    docs = db.collection('connectionHistory').where('connection.user_id', '==', user_id).stream()
+    connections = {doc.id: doc.to_dict() for doc in docs}
+    return dumps(connections)
+
+
+@app.route('/connection/add/<string:user_id>/<string:connection_dict>')
+def add_connection(user_id: str, connection_dict: dict | str):
+    if isinstance(connection_dict, str):
+        connection_dict = loads(connection_dict)
+
     user = User.from_dict(db.collection('users').document(user_id).get().to_dict())
     connection = Connection.from_dict(connection_dict)
 
@@ -54,6 +101,7 @@ def add_connection(user_id: str, connection_dict: dict):
     })
 
 
+@app.route('/connection/delete/<string:user_id>/<string:connection_id>')
 def delete_connection(user_id: str, connection_id: str):
     user = User.from_dict(db.collection('users').document(user_id).get().to_dict())
     docs = db.collection('connectionHistory').where('connection.id', '==', connection_id).stream()
@@ -68,17 +116,28 @@ def delete_connection(user_id: str, connection_id: str):
 
 
 # pending deprecation
-def update_on_survey_submit(connection_id: str, response: dict, inplace=False):
+@app.route('/connection/update/on_survey_submit/<string:connection_id>/<string:answers>/<int:inplace>')
+def update_on_survey_submit(connection_id: str, answers: dict | str, inplace=False):
+    if isinstance(answers, str):
+        answers = loads(answers)
+    if isinstance(inplace, int):
+        inplace = bool(inplace)
+
     update_connection(connection_id, {
-        'status': response['status'],
-        'hours_spent_together': firestore.firestore.Increment(response['additional_hours']),
-        'metrics': Connection.response_to_metrics(response)
+        'status': answers['status'],
+        'hours_spent_together': firestore.firestore.Increment(answers['additional_hours']),
+        'metrics': Connection.response_to_metrics(answers)
     }, inplace=inplace)
 
 
-def update_connection(connection_id: str, params: dict, inplace=False):
-    docs = db.collection('connectionHistory').where('connection.id', '==', connection_id).stream()
-    doc = max(docs, key=lambda x: x.to_dict()['timestamp'])
+@app.route('/connection/update/<string:connection_id>/<string:params>/<int:inplace>')
+def update_connection(connection_id: str, params: dict | str, inplace=False):
+    if isinstance(params, str):
+        loads(params)
+    if isinstance(inplace, int):
+        inplace = bool(inplace)
+
+    doc = _get_connection(connection_id)
 
     if inplace:
         changes = {f'connection.{k}': v for k, v in params.items()}
@@ -97,6 +156,7 @@ def update_connection(connection_id: str, params: dict, inplace=False):
     })
 
 
+@app.route('/connection/insights/<string:user_id>/<string:connection_id>')
 def get_insights_timeline(user_id: str, connection_id: str):
     docs = db.collection('connectionHistory').where('connection.id', '==', connection_id).stream()
 
