01cce7514a4906e3031aa19502bbbf4db8c7eb63
shaneSleeman <shanesleeman1@gmail.com>
2022-09-24 13:24:28 +1000

    Add dashboard

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,6 +1,50 @@
-//import "./Dashboard.css";
+import "./Dashboard.css";
 function Dashboard() {
-  return <>Dashboard</>;
+  return (
+    <>
+      <p className="title">Dashboard</p>
+      <div className="sideBar">
+        <div className="home sideBarButton"></div>
+        <div className="add sideBarButton"></div>
+        <div className="person sideBarButton"></div>
+        <div className="light sideBarButton"></div>
+        <div className="exit sideBarButton"></div>
+      </div>
+
+      <div className="container">
+        <form action="" className="parent">
+          <input
+            type="text"
+            className="search"
+            placeholder="search friends"
+          ></input>
+          <input type="button" name="" id="" className="btn"></input>
+        </form>
+      </div>
+
+      <div className="friend">
+        <img className="profilePic" src="src/proPic2.webp" alt=""></img>
+        <p className="name">Name</p>
+        <progress className="intimacy" max="100" value="70">
+          70%
+        </progress>
+      </div>
+      <div className="friend">
+        <img className="profilePic" src="src/proPic2.webp" alt=""></img>
+        <p className="name">Name</p>
+        <progress className="intimacy" max="100" value="70">
+          70%
+        </progress>
+      </div>
+      <div className="friend">
+        <img className="profilePic" src="src/proPic2.webp" alt=""></img>
+        <p className="name">Name</p>
+        <progress className="intimacy" max="100" value="70">
+          70%
+        </progress>
+      </div>
+    </>
+  );
 }
 
 export default Dashboard;
