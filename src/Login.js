fdc725f786fb3533ac8007717ff69beeefe176f4
shaneSleeman <shanesleeman1@gmail.com>
2022-09-24 13:13:25 +1000

    Add login page changes

--- src/Login.js
+++ src/Login.js
@@ -1,5 +1,6 @@
 import "./Login.css";
 import Logo from "./4.jpg";
+import Google from "./google.png";
 import * as HandleClick from "./HandleClick";
 function Login() {
   return (
@@ -7,15 +8,20 @@ function Login() {
       <img src={Logo} className="logo"></img>
       <div className="darkBox">
         <p className="message">Welcome to FSK!</p>
-        <input type="text" name="username" placeholder="Username"></input>
+        <input type="text" name="username" placeholder="Username/Email"></input>
         <input type="password" name="password" placeholder="Password"></input>
+        <p class="forget">Forgot password?</p>
         <bottonc className="button" onClick={HandleClick.handleSignIn}>
           Login
         </bottonc>
         <botton className="button">Signup</botton>
+        <botton class="google">
+          <img className="googleImg" src={Google} alt=""></img>
+        </botton>
       </div>
     </>
   );
 }
 
 export default Login;
+
