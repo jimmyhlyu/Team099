fcca9a5f2cadb5ded7630a62196c16a2a60ec802
shaneSleeman <shanesleeman1@gmail.com>
2022-09-24 14:03:58 +1000

    Set up flex in dashboard

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -4,39 +4,42 @@ import Profile2 from "./proPic2.webp";
 function Dashboard() {
   return (
     <div className="dashBody">
-      <p className="title">Dashboard</p>
+      <div className="dashTop">
+        <p className="title">Dashboard</p>
 
-      <div className="container">
-        <form action="" className="parent">
-          <input
-            type="text"
-            className="search"
-            placeholder="search friends"
-          ></input>
-          <input type="button" name="" id="" className="btn"></input>
-        </form>
+        <div className="container">
+          <form action="" className="parent">
+            <input
+              type="text"
+              className="search"
+              placeholder="search friends"
+            ></input>
+            <input type="button" name="" id="" className="btn"></input>
+          </form>
+        </div>
       </div>
-
-      <div className="friend">
-        <img className="profilePic" src={Profile1} alt=""></img>
-        <p className="name">Name</p>
-        <progress className="intimacy" max="100" value="70">
-          70%
-        </progress>
-      </div>
-      <div className="friend">
-        <img className="profilePic" src={Profile2} alt=""></img>
-        <p className="name">Name</p>
-        <progress className="intimacy" max="100" value="70">
-          70%
-        </progress>
-      </div>
-      <div className="friend">
-        <img className="profilePic" src={Profile2} alt=""></img>
-        <p className="name">Name</p>
-        <progress className="intimacy" max="100" value="70">
-          70%
-        </progress>
+      <div className="dashFriends">
+        <div className="friend">
+          <img className="profilePic" src={Profile1} alt=""></img>
+          <p className="name">Name</p>
+          <progress className="intimacy" max="100" value="70">
+            70%
+          </progress>
+        </div>
+        <div className="friend">
+          <img className="profilePic" src={Profile2} alt=""></img>
+          <p className="name">Name</p>
+          <progress className="intimacy" max="100" value="70">
+            70%
+          </progress>
+        </div>
+        <div className="friend">
+          <img className="profilePic" src={Profile2} alt=""></img>
+          <p className="name">Name</p>
+          <progress className="intimacy" max="100" value="70">
+            70%
+          </progress>
+        </div>
       </div>
     </div>
   );
