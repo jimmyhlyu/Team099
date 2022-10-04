74d615ec807a46e47583c9639ea3f75674d7b98e
shaneSleeman <shanesleeman1@gmail.com>
2022-10-04 12:41:56 +1100

    Add-friends formatting

--- src/AddFriend.js
+++ src/AddFriend.js
@@ -7,35 +7,39 @@ import pic6 from "./priPic6.jpeg";
 function AddFriend() {
   return (
     <>
-      <div className="title">Add Friends</div>
-      <div className="container">
-        <form action="" className="parent">
-          <input type="text" class="search" placeholder="add new friends" />
-          <input type="button" name="" id="" class="btn" />
-        </form>
-      </div>
-      <p className="recMes">Recommend Friend For You:</p>
-      <img className="refresh" src={Refresh} />
-      <div className="recFriend">
-        <img className="recPic" src={pic4} alt="" />
-        <p className="Name">John sendhard</p>
-        <p className="friendOf">4 mutual friend</p>
-        <div className="addBtn">Add</div>
-        <div className="ignoreBtn">Ignore</div>
-      </div>
-      <div className="recFriend">
-        <img className="recPic" src={pic5} alt="" />
-        <p className="Name">Aden thorm</p>
-        <p className="friendOf">2 mutual friend</p>
-        <div className="addBtn">Add</div>
-        <div className="ignoreBtn">Ignore</div>
-      </div>
-      <div className="recFriend">
-        <img className="recPic" src={pic6} alt="" />
-        <p className="Name">Andrea Migono</p>
-        <p className="friendOf">5 mutual friend</p>
-        <div className="addBtn">Add</div>
-        <div className="ignoreBtn">Ignore</div>
+      <div className="addFriend">
+        <div className="title">Add Friends</div>
+        <div className="container2">
+          <form action="" className="parent">
+            <input type="text" class="search" placeholder="add new friends" />
+            <input type="button" name="" id="" class="btn" />
+          </form>
+        </div>
+        <div classNamne="refreshLine">
+          <p className="recMes">Recommended friends for you:</p>
+          <img className="refresh" src={Refresh} />
+        </div>
+        <div className="recFriend">
+          <img className="recPic" src={pic4} alt="" />
+          <p className="Name">John sendhard</p>
+          <p className="friendOf">4 mutual friends</p>
+          <div className="addBtn">Add</div>
+          <div className="ignoreBtn">Ignore</div>
+        </div>
+        <div className="recFriend">
+          <img className="recPic" src={pic5} alt="" />
+          <p className="Name">Aden thorm</p>
+          <p className="friendOf">2 mutual friends</p>
+          <div className="addBtn">Add</div>
+          <div className="ignoreBtn">Ignore</div>
+        </div>
+        <div className="recFriend">
+          <img className="recPic" src={pic6} alt="" />
+          <p className="Name">Andrea Migono</p>
+          <p className="friendOf">5 mutual friends</p>
+          <div className="addBtn">Add</div>
+          <div className="ignoreBtn">Ignore</div>
+        </div>
       </div>
     </>
   );
