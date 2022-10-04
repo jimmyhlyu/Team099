5cb3eec487a50c6e24002925cf193f5954b5b33a
shaneSleeman <shanesleeman1@gmail.com>
2022-10-04 12:04:41 +1100

    Add add-friend style

--- src/AddFriend.js
+++ src/AddFriend.js
@@ -1,51 +1,8 @@
-import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
-
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+import "./AddFriend.css";
 
 function AddFriend() {
-  return (
-    <>
-      <Container>
-        <Row>
-          <Col></Col>
-          <Col sm={12} md={8} lg={6}>
-            <h1 className="text-center">NEW FRIEND</h1>
-            <Form>
-              <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Control type="email" placeholder="Name" />
-              </Form.Group>
-
-              <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Control type="password" placeholder="First Met At" />
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Control type="password" placeholder="First Met On" />
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Control type="password" placeholder="Contact Info" />
-              </Form.Group>
-              <Row style={{ justifyContent: "space-between" }}>
-                <Col sm md lg={1}>
-                  <Button variant="danger" type="submit">
-                    Clear
-                  </Button>
-                </Col>
-                <Col sm md lg={1}>
-                  <Button variant="success" type="submit">
-                    Submit
-                  </Button>
-                </Col>
-              </Row>
-            </Form>
-          </Col>
-          <Col></Col>
-        </Row>
-      </Container>
-    </>
-  );
+  return <></>;
 }
 
 export default AddFriend;
+
