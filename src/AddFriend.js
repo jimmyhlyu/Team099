fbb4b96f8259476516b72178b24bf5d73bf0aa18
shaneSleeman <shanesleeman1@gmail.com>
2022-10-17 12:20:54 +1100

    Add placeholder add friend page

--- src/AddFriend.js
+++ src/AddFriend.js
@@ -6,42 +6,21 @@ import pic6 from "./assets/priPic6.jpeg";
 
 function AddFriend() {
   return (
-    <>
-      <div className="addFriend">
-        <div className="title">Add Friends</div>
-        <div className="container2">
-          <form action="" className="parent">
-            <input type="text" class="search" placeholder="add new friends" />
-            <input type="button" name="" id="" class="btn" />
+    <div className="addFriend">
+      <div class="titleFriend">&nbsp;&nbsp;Add Friend</div>
+
+      <div class="signupFrm">
+        <div class="wrapper">
+          <form className="signForm" action="" class="form">
+            <div class="inputContainerSign">
+              <input type="text" name="name" class="inputSign" value=""></input>
+              <label class="labelSign">Name</label>
+            </div>
+            <input type="submit" class="submitBtn" value="Save"></input>
           </form>
         </div>
-        <div classNamne="refreshLine">
-          <p className="recMes">Recommended friends for you:</p>
-          <img className="refresh" src={Refresh} />
-        </div>
-        <div className="recFriend">
-          <img className="recPic" src={pic4} alt="" />
-          <p className="Name">John sendhard</p>
-          <p className="friendOf">4 mutual friends</p>
-          <div className="addBtn">Add</div>
-          <div className="ignoreBtn">Ignore</div>
-        </div>
-        <div className="recFriend">
-          <img className="recPic" src={pic5} alt="" />
-          <p className="Name">Aden thorm</p>
-          <p className="friendOf">2 mutual friends</p>
-          <div className="addBtn">Add</div>
-          <div className="ignoreBtn">Ignore</div>
-        </div>
-        <div className="recFriend">
-          <img className="recPic" src={pic6} alt="" />
-          <p className="Name">Andrea Migono</p>
-          <p className="friendOf">5 mutual friends</p>
-          <div className="addBtn">Add</div>
-          <div className="ignoreBtn">Ignore</div>
-        </div>
       </div>
-    </>
+    </div>
   );
 }
 
