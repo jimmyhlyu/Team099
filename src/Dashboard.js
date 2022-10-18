19224d1c11ed1560e1c7e1cf92cf5f3cbd2f3bb3
shaneSleeman <shanesleeman1@gmail.com>
2022-10-19 10:34:53 +1100

    Friend class

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -4,13 +4,21 @@ import Profile2 from "./assets/proPic2.jpeg";
 import Profile3 from "./assets/proPic2.webp";
 
 import FriendDash from "./FriendDash";
+import Friend from "./Friend";
+let friends = [Friend("Mason", 12, 12, 41)];
+
 function Dashboard() {
   return (
     <div className="addFriend">
       <div class="titleFriend">&nbsp;&nbsp;Dashboard</div>
 
       <div class="friendsList">
-        <FriendDash friendName="Lucy" fam="65" int="12" sim="42" />
+        <FriendDash
+          friendName={friends[0].friendName}
+          fam={friends[0].fam}
+          int={friends[0].int}
+          sim={friends[0].sim}
+        />
       </div>
 
       <div class="signupForm">
---
+++ src/Friend.js
@@ -0,0 +1,6 @@
+const Friend = (friendName, fam, int, sim) => {
+  return { friendName, fam, int, sim };
+};
+
+export default Friend;
+
