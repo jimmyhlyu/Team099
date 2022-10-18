5671e7e7fc84be918e88d175f9a4a209be36917b
shaneSleeman <shanesleeman1@gmail.com>
2022-10-19 10:12:43 +1100

    Friend dashboard component placeholder

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -2,10 +2,15 @@ import "./Dashboard.css";
 import Profile1 from "./assets/proPic3.jpeg";
 import Profile2 from "./assets/proPic2.jpeg";
 import Profile3 from "./assets/proPic2.webp";
+
+import FriendDash from "./FriendDash";
 function Dashboard() {
   return (
     <div className="addFriend">
       <div class="titleFriend">&nbsp;&nbsp;Dashboard</div>
+
+      <FriendDash />
+      <FriendDash />
 
       <div class="signupForm">
         <form className="signForm" action="">
---
+++ src/FriendDash.js
@@ -0,0 +1,9 @@
+function FriendDash() {
+  return (
+    <>
+      <h1>friend</h1>
+    </>
+  );
+}
+
+export default FriendDash;
