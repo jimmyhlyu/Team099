9716108726a69e1c9357a482b338cad86ed414b6
shaneSleeman <shanesleeman1@gmail.com>
2022-10-19 10:10:41 +1100

    Dashboard placeholder

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -4,112 +4,19 @@ import Profile2 from "./assets/proPic2.jpeg";
 import Profile3 from "./assets/proPic2.webp";
 function Dashboard() {
   return (
-    <div className="dashBody">
-      <div className="dashTop">
-        <p className="title">Dashboard (WIP)</p>
+    <div className="addFriend">
+      <div class="titleFriend">&nbsp;&nbsp;Dashboard</div>
 
-        <div className="dashContainer">
-          <form action="" className="parent">
-            <input
-              type="text"
-              className="search"
-              placeholder="search friends"
-            ></input>
-            <input type="button" name="" id="" className="btn"></input>
-          </form>
-        </div>
-      </div>
-      <div className="dashFriends">
-        <div className="friend">
-          <img className="profilePic" src={Profile1} alt=""></img>
-          <p className="name">Alice</p>
-          <progress className="intimacy" max="100" value="70">
-            70%
-          </progress>
-          <div>
-            <p className="text">
-              matric1 {"\n"} matric2 {"\n"} matric3
-            </p>
-          </div>
-          <progress
-            className="intimacy_1"
-            id="1"
-            max="100"
-            value="90"
-          ></progress>
-          <progress
-            className="intimacy_1"
-            id="2"
-            max="100"
-            value="80"
-          ></progress>
-          <progress
-            className="intimacy_1"
-            id="3"
-            max="100"
-            value="60"
-          ></progress>
-        </div>
-        <div className="friend">
-          <img className="profilePic" src={Profile2} alt=""></img>
-          <p className="name">Richard</p>
-          <progress className="intimacy" max="100" value="50">
-            50%
-          </progress>
-          <div>
-            <p className="text">
-              matric1 {"\n"} matric2 {"\n"} matric3
-            </p>
-          </div>
-          <progress
-            className="intimacy_1"
-            id="1"
-            max="100"
-            value="90"
-          ></progress>
-          <progress
-            className="intimacy_1"
-            id="2"
-            max="100"
-            value="80"
-          ></progress>
-          <progress
-            className="intimacy_1"
-            id="3"
-            max="100"
-            value="60"
-          ></progress>
-        </div>
-        <div className="friend">
-          <img className="profilePic" src={Profile3} alt=""></img>
-          <p className="name">Lucy</p>
-          <progress className="intimacy" max="100" value="95">
-            95%
-          </progress>
-          <div>
-            <p className="text">
-              matric1 {"\n"} matric2 {"\n"} matric3
-            </p>
-          </div>
-          <progress
-            className="intimacy_1"
-            id="1"
-            max="100"
-            value="90"
-          ></progress>
-          <progress
-            className="intimacy_1"
-            id="2"
-            max="100"
-            value="80"
-          ></progress>
-          <progress
-            className="intimacy_1"
-            id="3"
-            max="100"
-            value="60"
-          ></progress>
-        </div>
+      <div class="signupForm">
+        <form className="signForm" action="">
+          <input
+            type="text"
+            name="name"
+            class="inputSign"
+            placeholder="Name"
+          ></input>
+          <input type="submit" class="submitBtn2" value="Add"></input>
+        </form>
       </div>
     </div>
   );
