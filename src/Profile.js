e9bd53da7fe531a5d9ad1c677faffba18e1b46dd
shaneSleeman <shanesleeman1@gmail.com>
2022-10-19 15:45:57 +1100

    Complete edit profile page

--- src/Profile.js
+++ src/Profile.js
@@ -7,45 +7,52 @@ import age from "./assets/age.png";
 import email from "./assets/email.png";
 import edit from "./assets/editPicBtn.png";
 
+// Note: Uses same css as AddFriend
+
 function Profile() {
   return (
-    <div className="profileBody">
-      <div className="profile">&nbsp;&nbsp;User Profile (WIP)</div>
-      <div className="profilePic2">
-        <button className="editPic">
-          <img src={edit} />
-        </button>
-      </div>
-      <p className="name">Shala Alice</p>
-      <div className="profileInfo">
-        <img className="img income" src={income} alt="" />
-        <img className="img phone" src={phone} alt="" />
-        <img className="img friend" src={friendship} alt="" />
-        <img className="img age" src={age} alt="" />
-        <img className="img email" src={email} alt="" />
-        <p className="subTitle AB">About</p>
-        <div className="about blockStyle">
-          <p className="titleInfo">Age:</p>
-          <p className="info">28</p>
-          <p className="titleInfo">Income:</p>
-          <p className="info">Swimming Shopping</p>
-          <p className="titleInfo">Email:</p>
-          <p className="info">shala_alice@gmail.com8</p>
-          <p className="titleInfo">Number#:</p>
-          <p className="info">0483728194</p>
-          <p className="titleInfo">Close Friend with:</p>
-          <p className="info">Alex, Bob, Aden</p>
-        </div>
-        <p className="subTitle SD">Self Description</p>
-        <div className="SD blockStyle">
-          &nbsp;&nbsp;&nbsp;&nbsp;I am an optimistic, candid, responsible and
-          social person. I am an optimistic, candid, responsible and social
-          person. I am confident with my thinking analysis that I can convince
-          people with my points. I am self-reliant, well behaved and above all,
-          a person of strong character. I take initiative whenever the situation
-          arises and come off with flying colours.
-        </div>
-        <p className="subTitle per">Personality</p>
+    <div className="addFriend2">
+      <div class="titleFriend2">&nbsp;&nbsp;Edit Profile</div>
+
+      <div class="signupForm2">
+        <form className="signForm2" action="">
+          <input
+            type="text"
+            name="name"
+            class="inputSign"
+            placeholder="Name"
+          ></input>
+          <input
+            type="number"
+            name="age"
+            class="inputSign"
+            placeholder="Age"
+          ></input>
+
+          <div class="radio">
+            <input
+              type="checkbox"
+              id="friendship"
+              name="friendship"
+              value="Bike"
+            />
+            <label for="friendship">Looking for Friendship</label>
+          </div>
+          <div class="radio">
+            <input
+              type="checkbox"
+              id="relationship"
+              name="relationship"
+              value="Car"
+            />
+            <label for="relationship">Looking for Relationship</label>
+          </div>
+          <div class="radio">
+            <input type="checkbox" id="career" name="career" value="Boat" />
+            <label for="career">Loking for Career</label>
+          </div>
+          <input type="submit" class="submitBtn22" value="Save"></input>
+        </form>
       </div>
     </div>
   );
