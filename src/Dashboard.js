4b9b5e17b993a6c51614574a05f60f2bc20a8606
shaneSleeman <shanesleeman1@gmail.com>
2022-10-19 13:14:34 +1100

    Fix colours

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -7,12 +7,12 @@ import FriendDash from "./FriendDash";
 import Friend from "./Friend";
 import useState, { useEffect } from "react";
 let friends = [
-  Friend("Mason", 12, 12, 41, 1),
-  Friend("Lucy", 15, 52, 42, 2),
-  Friend("Lucy", 15, 52, 42, 3),
-  Friend("Lucy", 15, 52, 42, 4),
-  Friend("Lucy", 15, 52, 42, 5),
-  Friend("Lucy", 15, 52, 42, 6),
+  Friend("Jon", "12", "25", "75", 1),
+  Friend("Lucy", "15", "52", "42", 2),
+  Friend("Bob", "15", "12", "42", 3),
+  Friend("Lucy2", "15", "100", "46", 4),
+  Friend("John", "15", "12", "12", 5),
+  Friend("Test", "15", "32", "76", 6),
 ];
 
 function Dashboard() {
