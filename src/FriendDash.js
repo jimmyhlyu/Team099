59bb56c94c8e476bad3322e5898846c517bd5687
shaneSleeman <shanesleeman1@gmail.com>
2022-10-19 13:07:44 +1100

    Complete survey draft

--- src/FriendDash.js
+++ src/FriendDash.js
@@ -4,7 +4,6 @@ import editBtn from "./assets/editBtn.png";
 import infoBtn from "./assets/infoBtn.png";
 import deleteBtn from "./assets/deleteBtn.png";
 
-import SurveyPopup from "./SurveyPopup";
 import Popup from "reactjs-popup";
 
 function FriendDash({ friendName, fam, int, sim, id }) {
@@ -41,7 +40,408 @@ function FriendDash({ friendName, fam, int, sim, id }) {
             />
           }
         >
-          <h1>test {id}</h1>
+          <div class="survey">
+            {/* Post survey with {id} */}
+            <h3 class="surveyTitle">
+              What have you observed in {friendName} during your most recent
+              encounter?
+            </h3>
+            <form action="" method="post">
+              <div class="question">
+                <h4 class="questionTitle">Charm</h4>
+                <div>
+                  <input type="radio" id="-2" name="charm" value="-2" />
+                  <label for="-2" class="surveyTitle">
+                    -2
+                  </label>
+                </div>
+
+                <div>
+                  <input type="radio" id="-1" name="charm" value="-1" />
+                  <label for="-1" class="surveyTitle">
+                    -1
+                  </label>
+                </div>
+
+                <div>
+                  <input type="radio" id="0" name="charm" value="0" />
+                  <label for="0" class="surveyTitle">
+                    0
+                  </label>
+                </div>
+
+                <div>
+                  <input type="radio" id="1" name="charm" value="1" />
+                  <label for="1" class="surveyTitle">
+                    1
+                  </label>
+                </div>
+
+                <div>
+                  <input type="radio" id="2" name="charm" value="2" />
+                  <label for="2" class="surveyTitle">
+                    2
+                  </label>
+                </div>
+              </div>
+
+              <div class="question">
+                <h4 class="questionTitle">Closeness</h4>
+                <div>
+                  <input type="radio" id="-2" name="closeness" value="-2" />
+                  <label for="-2" class="surveyTitle">
+                    -2
+                  </label>
+                </div>
+
+                <div>
+                  <input type="radio" id="-1" name="closeness" value="-1" />
+                  <label for="-1" class="surveyTitle">
+                    -1
+                  </label>
+                </div>
+
+                <div>
+                  <input type="radio" id="0" name="closeness" value="0" />
+                  <label for="0" class="surveyTitle">
+                    0
+                  </label>
+                </div>
+
+                <div>
+                  <input type="radio" id="1" name="closeness" value="1" />
+                  <label for="1" class="surveyTitle">
+                    1
+                  </label>
+                </div>
+
+                <div>
+                  <input type="radio" id="2" name="closeness" value="2" />
+                  <label for="2" class="surveyTitle">
+                    2
+                  </label>
+                </div>
+              </div>
+
+              <div class="question">
+                <h4 class="questionTitle">Companionship</h4>
+                <div>
+                  <input type="radio" id="-2" name="companionship" value="-2" />
+                  <label for="-2" class="surveyTitle">
+                    -2
+                  </label>
+                </div>
+
+                <div>
+                  <input type="radio" id="-1" name="companionship" value="-1" />
+                  <label for="-1" class="surveyTitle">
+                    -1
+                  </label>
+                </div>
+
+                <div>
+                  <input type="radio" id="0" name="companionship" value="0" />
+                  <label for="0" class="surveyTitle">
+                    0
+                  </label>
+                </div>
+
+                <div>
+                  <input type="radio" id="1" name="companionship" value="1" />
+                  <label for="1" class="surveyTitle">
+                    1
+                  </label>
+                </div>
+
+                <div>
+                  <input type="radio" id="2" name="companionship" value="2" />
+                  <label for="2" class="surveyTitle">
+                    2
+                  </label>
+                </div>
+              </div>
+
+              <div class="question">
+                <h4 class="questionTitle">Usefulness</h4>
+                <div>
+                  <input type="radio" id="-2" name="usefulness" value="-2" />
+                  <label for="-2" class="surveyTitle">
+                    -2
+                  </label>
+                </div>
+
+                <div>
+                  <input type="radio" id="-1" name="usefulness" value="-1" />
+                  <label for="-1" class="surveyTitle">
+                    -1
+                  </label>
+                </div>
+
+                <div>
+                  <input type="radio" id="0" name="usefulness" value="0" />
+                  <label for="0" class="surveyTitle">
+                    0
+                  </label>
+                </div>
+
+                <div>
+                  <input type="radio" id="1" name="usefulness" value="1" />
+                  <label for="1" class="surveyTitle">
+                    1
+                  </label>
+                </div>
+
+                <div>
+                  <input type="radio" id="2" name="usefulness" value="2" />
+                  <label for="2" class="surveyTitle">
+                    2
+                  </label>
+                </div>
+              </div>
+
+              <div class="question">
+                <h4 class="questionTitle">Attraction</h4>
+                <div>
+                  <input type="radio" id="-2" name="attraction" value="-2" />
+                  <label for="-2" class="surveyTitle">
+                    -2
+                  </label>
+                </div>
+
+                <div>
+                  <input type="radio" id="-1" name="attraction" value="-1" />
+                  <label for="-1" class="surveyTitle">
+                    -1
+                  </label>
+                </div>
+
+                <div>
+                  <input type="radio" id="0" name="attraction" value="0" />
+                  <label for="0" class="surveyTitle">
+                    0
+                  </label>
+                </div>
+
+                <div>
+                  <input type="radio" id="1" name="attraction" value="1" />
+                  <label for="1" class="surveyTitle">
+                    1
+                  </label>
+                </div>
+
+                <div>
+                  <input type="radio" id="2" name="attraction" value="2" />
+                  <label for="2" class="surveyTitle">
+                    2
+                  </label>
+                </div>
+              </div>
+
+              <div class="question">
+                <h4 class="questionTitle">Responsiveness</h4>
+                <div>
+                  <input
+                    type="radio"
+                    id="-2"
+                    name="responsiveness"
+                    value="-2"
+                  />
+                  <label for="-2" class="surveyTitle">
+                    -2
+                  </label>
+                </div>
+
+                <div>
+                  <input
+                    type="radio"
+                    id="-1"
+                    name="responsiveness"
+                    value="-1"
+                  />
+                  <label for="-1" class="surveyTitle">
+                    -1
+                  </label>
+                </div>
+
+                <div>
+                  <input type="radio" id="0" name="responsiveness" value="0" />
+                  <label for="0" class="surveyTitle">
+                    0
+                  </label>
+                </div>
+
+                <div>
+                  <input type="radio" id="1" name="responsiveness" value="1" />
+                  <label for="1" class="surveyTitle">
+                    1
+                  </label>
+                </div>
+
+                <div>
+                  <input type="radio" id="2" name="responsiveness" value="2" />
+                  <label for="2" class="surveyTitle">
+                    2
+                  </label>
+                </div>
+              </div>
+
+              <div class="question">
+                <h4 class="questionTitle">Toxicity</h4>
+                <div>
+                  <input type="radio" id="-2" name="toxicity" value="-2" />
+                  <label for="-2" class="surveyTitle">
+                    -2
+                  </label>
+                </div>
+
+                <div>
+                  <input type="radio" id="-1" name="toxicity" value="-1" />
+                  <label for="-1" class="surveyTitle">
+                    -1
+                  </label>
+                </div>
+
+                <div>
+                  <input type="radio" id="0" name="toxicity" value="0" />
+                  <label for="0" class="surveyTitle">
+                    0
+                  </label>
+                </div>
+
+                <div>
+                  <input type="radio" id="1" name="toxicity" value="1" />
+                  <label for="1" class="surveyTitle">
+                    1
+                  </label>
+                </div>
+
+                <div>
+                  <input type="radio" id="2" name="toxicity" value="2" />
+                  <label for="2" class="surveyTitle">
+                    2
+                  </label>
+                </div>
+              </div>
+
+              <div class="question">
+                <h4 class="questionTitle">Friction</h4>
+                <div>
+                  <input type="radio" id="-2" name="friction" value="-2" />
+                  <label for="-2" class="surveyTitle">
+                    -2
+                  </label>
+                </div>
+
+                <div>
+                  <input type="radio" id="-1" name="friction" value="-1" />
+                  <label for="-1" class="surveyTitle">
+                    -1
+                  </label>
+                </div>
+
+                <div>
+                  <input type="radio" id="0" name="friction" value="0" />
+                  <label for="0" class="surveyTitle">
+                    0
+                  </label>
+                </div>
+
+                <div>
+                  <input type="radio" id="1" name="friction" value="1" />
+                  <label for="1" class="surveyTitle">
+                    1
+                  </label>
+                </div>
+
+                <div>
+                  <input type="radio" id="2" name="friction" value="2" />
+                  <label for="2" class="surveyTitle">
+                    2
+                  </label>
+                </div>
+              </div>
+
+              <div class="question">
+                <h4 class="questionTitle">Nexus</h4>
+                <div>
+                  <input type="radio" id="-2" name="nexus" value="-2" />
+                  <label for="-2" class="surveyTitle">
+                    -2
+                  </label>
+                </div>
+
+                <div>
+                  <input type="radio" id="-1" name="nexus" value="-1" />
+                  <label for="-1" class="surveyTitle">
+                    -1
+                  </label>
+                </div>
+
+                <div>
+                  <input type="radio" id="0" name="nexus" value="0" />
+                  <label for="0" class="surveyTitle">
+                    0
+                  </label>
+                </div>
+
+                <div>
+                  <input type="radio" id="1" name="nexus" value="1" />
+                  <label for="1" class="surveyTitle">
+                    1
+                  </label>
+                </div>
+
+                <div>
+                  <input type="radio" id="2" name="nexus" value="2" />
+                  <label for="2" class="surveyTitle">
+                    2
+                  </label>
+                </div>
+              </div>
+
+              <div class="question">
+                <h4 class="questionTitle">Popularity</h4>
+                <div>
+                  <input type="radio" id="-2" name="popularity" value="-2" />
+                  <label for="-2" class="surveyTitle">
+                    -2
+                  </label>
+                </div>
+
+                <div>
+                  <input type="radio" id="-1" name="popularity" value="-1" />
+                  <label for="-1" class="surveyTitle">
+                    -1
+                  </label>
+                </div>
+
+                <div>
+                  <input type="radio" id="0" name="popularity" value="0" />
+                  <label for="0" class="surveyTitle">
+                    0
+                  </label>
+                </div>
+
+                <div>
+                  <input type="radio" id="1" name="popularity" value="1" />
+                  <label for="1" class="surveyTitle">
+                    1
+                  </label>
+                </div>
+
+                <div>
+                  <input type="radio" id="2" name="popularity" value="2" />
+                  <label for="2" class="surveyTitle">
+                    2
+                  </label>
+                </div>
+              </div>
+
+              <button type="submit" class="submitBtn22">
+                Submit
+              </button>
+            </form>
+          </div>
         </Popup>
         {/*
         <img
